29e18fdb249df6160506a17502b404113515b16c
misa-takeba <e1416mt@aiit.ac.jp>
2014-07-26 00:01:29 +0900

    Finish user signup

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -36,4 +36,7 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  # bcrypt'のコスト関数を下げることでテストの速度を向上させる。
+  ActiveModel::SecurePassword.min_cost = true
 end
