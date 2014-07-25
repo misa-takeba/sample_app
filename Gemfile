eb26cd901d65a87440301db8ff2bae4f24b86898
misa-takeba <e1416mt@aiit.ac.jp>
2014-07-26 08:32:14 +0900

    Finish sign in

--- Gemfile
+++ Gemfile
@@ -27,7 +27,8 @@ group :test do
   gem 'growl', '1.0.3'
 
   gem 'factory_girl_rails','4.2.1'
-
+  gem 'cucumber-rails','1.4.0',:require => false
+  gem 'database_cleaner', github:'bmabey/database_cleaner'
   # Uncomment these lines on Linux.
   # gem 'libnotify', '0.8.0'
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,6 +13,7 @@ describe User do
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
+  it { should respond_to(:remember_token) }
   it { should respond_to(:authenticate) }
 
   it { should be_valid }
@@ -102,5 +103,10 @@ describe User do
       let(:user_for_invalid_password) { found_user.authenticate("invalid") }
       specify { expect(user_for_invalid_password).to be_false }
     end
+  end
+
+  describe "remember token" do
+   before { @user.save }
+   its(:remember_token) { should_not be_blank }
   end
 end
